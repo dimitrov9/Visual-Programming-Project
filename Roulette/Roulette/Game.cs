0364f5e62d2fb2674b4caf7855f62888b1ee843d
Nikola Dimitrov <dimitrov333@hotmail.com>
2017-05-16 22:44:27 +0200

    Everything else
    The finish of the functionality of the Roulette with a random number
    from 0 to 36 without the graphical wheel

--- Roulette/Roulette/Game.cs
+++ Roulette/Roulette/Game.cs
@@ -22,11 +22,30 @@ namespace Roulette
         ///</summary>
         public static long[] numBets = new long[37];
 
+        public static long first12;
+        public static long second12;
+        public static long third12;
+        ///<summary>
+        ///0-6 Array contains the credits of the following bets:<br/>
+        ///0. 1-18 <br/>
+        ///1. 19-36 <br/>
+        ///2. even <br/>
+        ///3 odd<br/>
+        ///4. red  <br/>
+        ///5. black
+        ///</summary>
+        public static long[] otherBets = new long[6];
 
-        private static long prevTotalBet;
-        private static long[] prevNumBets = new long[37];
-        
 
+        public static long prevTotalBet;
+        public static long[] prevNumBets = new long[37];
+        public static long prevFirst12;
+        public static long prevSecond12;
+        public static long prevThird12;
+        public static long[] prevOtherBets = new long[6];
+
+        private static int[] redNumbers = new int[18]{ 2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33 , 35};
+        private static int[] blackNumbers = new int[18] {  1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34 ,36 };
         // Initializes a game that takes one argumet of Credits how much the player should start with and the other are 0
         //public static Game()
         //{
@@ -59,17 +78,55 @@ namespace Roulette
         public static int ReturnProfitLoss(int nextGuess)
         {
             int profit = 0;
-            if (nextGuess == 1)
-            {
-                 profit = (Int32)(numBets[1] * 36 - totalBet);
+            profit = (int)(numBets[nextGuess] * 36 );
+
+            if(nextGuess>0){
+
+                 if (nextGuess <= 12)
+                {
+                    profit += (int)(first12 * 3);
+                }
+                else if (nextGuess > 12 && nextGuess <= 24)
+                {
+                    profit += (int)(second12 * 3);
+                }
+                else if (nextGuess > 24 && nextGuess <= 36)
+                {
+                    profit += (int)(third12 * 3);
+                }
+
+
+                if ( nextGuess < 19)
+                {
+                    profit += (int)(otherBets[0] * 2);
+                }
+                else if (nextGuess > 18 && nextGuess < 37)
+                {
+                    profit += (int)(otherBets[1] * 2);
+                }
+
+
+                if (nextGuess % 2 == 0)
+                {
+                    profit += (int)(otherBets[2] * 2);
+                }
+                else if (nextGuess % 2 == 1)
+                {
+                    profit += (int)(otherBets[3] * 2);
+                }
+
+                if (isRed(nextGuess))
+                {
+                    profit += (int)(otherBets[4] * 2);
+                }
+                else if (isBlack(nextGuess))
+                {
+                    profit += (int)(otherBets[5] * 2);
+                }
+
             }
-
-
-            if (profit > 0)
-            {
-                credits += profit;
-            }
-
+           
+            credits += profit;
             return profit;
         }
 
@@ -83,17 +140,49 @@ namespace Roulette
             {
                 numBets[i] = 0L;
             }
+            first12 = 0;
+            second12 = 0;
+            third12 = 0;
+            for (int i = 0; i < otherBets.Length; i++)
+            {
+                otherBets[i] = 0;
+            }
 
         }
 
         ///<summary>
         ///Takes a number and adds 5 to the numBets array at a index of that number, updates totalBet and credits.
         ///</summary>
-        public static void InstertNumberBet(int number)
+        public static void InstertNumberBet(int number,int betAmount)
         {
-            numBets[number] += 5;
-            totalBet += 5;
-            credits -= 5;
+            numBets[number] += betAmount;
+            totalBet += betAmount;
+            credits -= betAmount;
+        }
+
+        public static void InsertTwelves(int number, int betAmount)
+        {
+            if (number == 1)
+            {
+                first12 += betAmount;
+            }
+            else if (number == 2)
+            {
+                second12 += betAmount;
+            }
+            else if (number == 3)
+            {
+                third12 += betAmount;
+            }
+            totalBet += betAmount;
+            credits -= betAmount;
+        }
+
+        public static void InsertOtherBets(int number, int betAmount)
+        {
+            otherBets[number] += betAmount;
+            totalBet += betAmount;
+            credits -= betAmount;
         }
 
         ///<summary>
@@ -105,6 +194,13 @@ namespace Roulette
             for (int i = 0; i < 37; i++)
             {
                 prevNumBets[i] = numBets[i];
+            }
+            prevFirst12 = first12;
+            prevSecond12 = second12;
+            prevThird12 = third12;
+            for (int i = 0; i < 6; i++)
+            {
+                prevOtherBets[i] = otherBets[i];
             }
         }
 
@@ -118,7 +214,80 @@ namespace Roulette
             {
                 numBets[i] = prevNumBets[i];
             }
+            first12 = prevFirst12;
+            second12 = prevSecond12;
+            third12 = prevThird12;
+            for (int i = 0; i < 6; i++)
+            {
+                otherBets[i] = prevOtherBets[i];
+            }
             credits -= totalBet;
+        }
+
+        ///<summary>
+        ///Resets the bets to 0 and returns credit
+        ///</summary>
+        public static void CancelAllBets()
+        {
+            credits += totalBet;
+            totalBet = 0;
+            for (int i = 0; i < 37; i++)
+            {
+                numBets[i] = 0;
+            }
+            first12 = 0;
+            second12 = 0;
+            third12 = 0;
+            for (int i = 0; i < 6; i++)
+            {
+                otherBets[i] = 0;
+            }
+        }
+
+        ///<summary>
+        ///Doubles all the bets
+        ///</summary>
+        public static void DoubleAllBets()
+        {
+            credits -= totalBet;
+            totalBet *= 2;
+            for (int i = 0; i < 37; i++)
+            {
+                numBets[i] *= 2;
+            }
+            first12 *= 2;
+            second12 *= 2;
+            third12 *= 2;
+            for (int i = 0; i < 6; i++)
+            {
+                otherBets[i] *= 2;
+            }
+
+        }
+
+
+        private static bool isRed(int number)
+        {
+            foreach (int red in redNumbers)
+            {
+                if (number == red)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static bool isBlack(int number)
+        {
+            foreach (int black in blackNumbers)
+            {
+                if (number == black)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
---
+++ Roulette/Roulette/SaveAndExit.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Roulette
+{
+    public partial class SaveAndExit : Form
+    {
+        public SaveAndExit()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Yes;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.No;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+        }
+    }
+}
